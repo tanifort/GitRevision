8699f9c0f304254906dac99a0702818a952a96dc
raymond <tanifor.tamah@gmail.com>
2018-08-24 15:44:24 +0200

    Created a person Object in The app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var Person={
+    name="belmo Ray";
+    Id=01;
+    run= function(){
+    console.log("Run Start----------->");
+    console.log("Run End------------>");
+    console.log("38Km/h");
+    
+}
+}
