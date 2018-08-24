68a1c122764178abdae1c37e8ff7484cd1ee2452
raymond <tanifor.tamah@gmail.com>
2018-08-24 22:02:08 +0200

    New Class

---
+++ HelloWorld/src/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.HelloWorld;
+
+/**
+ * @author Ray
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
